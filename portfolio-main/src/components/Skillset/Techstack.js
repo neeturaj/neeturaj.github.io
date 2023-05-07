6fae7f4b6f23d1f9ebe63a197d503b8054484499
Neeturaj.Pashedla <neeturaj.970@gmail.com>
2023-05-07 22:38:48 +0530

    files added

---
+++ portfolio-main/src/components/Skillset/Techstack.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiJava,
+  DiGit,
+  DiBootstrap,
+  DiCss3
+} from "react-icons/di";
+import {
+
+
+  SiHtml5
+} from "react-icons/si";
+
+const Techstack = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
