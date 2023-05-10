89d1d30f016bc795fe497333bac2b70208d71f24
Neeturaj.Pashedla <neeturaj.970@gmail.com>
2023-05-10 20:10:18 +0530

    files changes

--- SpringDataJpa/src/main/java/com/example/demo/springdatajpa/repository/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.springdatajpa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.springdatajpa.data.entity.Products;
-
-public interface ProductsRepository extends CrudRepository<Products, Long> {
-
-}
--- SpringDataJpa/src/main/java/com/example/demo/springdatajpa/services/SalesService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.springdatajpa.services;
-
-import com.example.demo.springdatajpa.data.entity.Sales;
-
-public interface SalesService {
-	 Sales getSale(int id);
-}
